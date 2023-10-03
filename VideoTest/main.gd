5a09c8889558dce245bd00dfe0862c4218317c29
Marc d'Eon <highlyderivative.games@gmail.com>
2023-10-03 12:28:06 -0400

    Initial commit

---
+++ VideoTest/main.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var homePanel = $HBoxContainer/HomePanel
+@onready var settingsPanel = $HBoxContainer/SettingsPanel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Start with the home panel visible and the side panel minimized
+	_on_home_pressed()
+	_on_hamburger_toggled(false)
+
+
+# Toggle visibility of side pannel buttons
+func _on_hamburger_toggled(button_pressed):
+	for child in $HBoxContainer/SidePanel/VBoxContainer.get_children():
+		if child != $HBoxContainer/SidePanel/VBoxContainer/Hamburger:
+			child.visible = button_pressed
+
+
+# Enable the home panel and disable all others
+func _on_home_pressed():
+	homePanel.visible = true
+	settingsPanel.visible = false
+
+
+# Enable the settings pannel and disable all others
+func _on_settings_pressed():
+	homePanel.visible = false
+	settingsPanel.visible = true
