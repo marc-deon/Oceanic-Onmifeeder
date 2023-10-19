dc991167024df306253df451b4ae679055f5dfb7
Tim <tmoor209@students.kennesaw.edu>
2023-10-19 19:31:55 -0400

    Moved some stuff around, added Stream option

--- rudp/rudp_port.py
+++ rudp/rudp_port.py
@@ -2,7 +2,9 @@
 from .rudp_message import RudpMessage
 from .rudp_exceptions import RudpTimeout
 from socket_convenience import BUFF_SIZE
-
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from rudp import RudpConnection
 
 class RudpPort:
     """(Virtual) RUDP port"""
@@ -11,56 +13,26 @@ class RudpPort:
         return self.parent.socket
 
 
-    def __init__(self, port:int, peerport:int, parent:'RudpConnection') -> 'RudpPort':
+    def __init__(self, port:int, peerport:int, parent:'RudpConnection', stream:bool=False) -> 'RudpPort':
         self.port:int = port
         self.peerPort:int = peerport
         self.parent:'RudpConnection' = parent
         self.queue:list[RudpMessage] = []
         self.lastId:int = 0
+        self.stream:bool = stream
 
 
-    def Send(self, msg:str, system:bool=False) -> None:
+    def Send(self, msg:str) -> None:
         """Send a message optionally marked as a system message."""
-        # TODO: We should maybe have a private _SendSystem instead for this.
-        self.parent.Send(self, msg, system)
+        self.parent._Send(self, msg, system=False)
+
+
+    def _SendSystem(self, msg:str) -> None:
+        self.parent._Send(self, msg, system=True)
+        pass
 
 
     def Receive(self) -> RudpMessage:
         """Wait for message, return it and send an ACK. Also queue message for other ports in the mean time."""
-
-        incoming = False
-        # Check queue
-        if len(self.queue) > 0:
-            incoming = self.queue.pop(0)
-
-        else:
-            attempts = 0
-            while attempts < self.parent.MAX_ATTEMPTS:
-                try:
-                    msg, (ip, addr) = self.socket.recvfrom(BUFF_SIZE)
-                    if (ip, addr) != self.parent.peer:
-                        # This is a stranger, ignore them
-                        continue
-
-                    incoming = RudpMessage.Decode(msg)
-
-                    if (not incoming.system) or (incoming.string != "ACK"):
-                        # The only time we're not going to send ACK is when the message we receive
-                        # it IS a system message AND the message itself is ACK
-                        self.parent._SendAck(incoming)
-
-                    if incoming.destPort != self.port:
-                        # Add to other port's queue
-                        self.parent.ports[incoming.destPort].queue.append(incoming)
-
-                    if not incoming.system:
-                        break
-
-                except TimeoutError:
-                    attempts += 1
-
-            if attempts == self.parent.MAX_ATTEMPTS:
-                raise RudpTimeout()
-
-        assert(incoming)
-        return incoming
+        return self.parent._Receive(self)
+        
