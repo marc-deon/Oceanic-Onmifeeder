d3e14bc77dcae6eee02faf2c61b657cc151bb662
Timothy Moore <tim_e_moore@bellsouth.net>
2023-10-24 22:23:18 -0400

    Made holepunch functions in socket_convenience.py

--- enet-chat-test.py
+++ enet-chat-test.py
@@ -1,93 +1,31 @@
 #! /usr/bin/env python3
 # https://github.com/aresch/pyenet
 
+import curses
+import enet
+import enet_chatroom
+import socket_convenience as sc
+
 from sys import argv
 
 hostOrConnect = argv[1]
 username = argv[2]
 
-import enet
-import enet_chatroom
-import curses
-import socket_convenience as sc
-
-# Holepunch server address and port
-SERVER_ADDR = ("highlyderivative.games", 4800)
 # Username to use as a key for holepunching. Will be phased out later for something more secure.
 HOLEPUNCH_CODE = "poseidon"
 
-# Create a socket, open a random port
-hp_sock = sc.CreateSocket()
-sc.utf8send(hp_sock, "FRSH", SERVER_ADDR)
-ourPort = sc.GetSocketPort(hp_sock)
-
 if hostOrConnect == "host":
-    # Request a spot in the holepunch server
-    sc.utf8send(hp_sock, f"HOST {sc.GetLocalIp()} {HOLEPUNCH_CODE} {ourPort}", SERVER_ADDR)
-    msg = sc.utf8get(hp_sock, True)[0]
-
-    while True:
-        try:
-            msg = sc.utf8get(hp_sock, True)[0]
-
-            match msg:
-                case ["HOSTING", public]:
-                    print("Hosting at...", sc.GetLocalIp(), public)
-
-                case ["EXPECT", clientaddr, clientlocal, clientport, clientlocalport]:
-                    # Find our peer
-                    peerIp, peerPort, ourIp, ourPort = sc.holepunch(hp_sock, clientaddr, clientlocal, clientport, clientlocalport)
-                    # We don't need this anymore
-                    hp_sock.close()
-                    # enet stuff
-                    host = enet.Host(enet.Address(ourIp, ourPort), peerCount=2)
-                    peer = host.connect(enet.Address(peerIp, peerPort), channelCount=1)
-                    # Start chatroom
-                    cr = enet_chatroom.Chatroom(host, peer, 0, username)
-                    curses.wrapper(cr.main)
-                    break
-
-                case ["OK"]:
-                    pass
-
-                case ["HAND1"]:
-                    # Holepunching junk; it's fine
-                    pass
-
-                case _:
-                    print("Invalid response", msg)
-
-        except TimeoutError:
-            # Refresh the port
-            sc.utf8send(hp_sock, "FRSH", SERVER_ADDR)
-            continue
+    # Find our peer
+    hp_sock = sc.register_for_holepunch(HOLEPUNCH_CODE)
+    peerIp, peerPort, ourIp, ourPort = sc.wait_for_holepunch(hp_sock)
 
 if hostOrConnect == "connect":
+    # Find our peer
+    peerIp, peerPort, ourIp, ourPort = sc.connect_to_holepunch(HOLEPUNCH_CODE)
 
-    # Send message to holepunch server
-    sc.utf8send(hp_sock, f"CONN {sc.GetLocalIp()} {HOLEPUNCH_CODE} {ourport}", SERVER_ADDR)
-
-    # Listen for response
-    while True:
-        msg, addr, port = sc.utf8get(hp_sock, True)
-        match msg:
-            # Recieved message with our peer's info
-            case ["CONNTO", hostaddr, hostlocal, hostport, hostlocalport]:
-                # Find our peer
-                peerIp, peerPort = sc.holepunch(hp_sock, hostaddr, hostlocal, hostport, hostlocalport)
-                # We don't need this anymore
-                hp_sock.close()
-                # enet stuff
-                host = enet.Host(enet.Address(None, ourport), peerCount=2)
-                peer = host.connect(enet.Address(peerIp, peerPort), channelCount=1)
-                # Start chatroom
-                cr = enet_chatroom.Chatroom(host, peer, 0, username)
-                curses.wrapper(cr.main)
-                break
-
-            case ["HAND1"]:
-                # Holepunching junk; it's fine
-                pass
-            
-            case _:
-                print("Invalid message", msg)
+# enet stuff
+host = enet.Host(enet.Address(ourIp, ourPort), peerCount=2)
+peer = host.connect(enet.Address(peerIp, peerPort), channelCount=1)
+# Start chatroom
+cr = enet_chatroom.Chatroom(host, peer, 0, username)
+curses.wrapper(cr.main)
