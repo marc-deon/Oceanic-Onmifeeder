dd9c7fe7cd1fe1e580fd8541f0174a393ca70be8
Timothy Moore <tmoor209@students.kennesaw.edu>
2023-11-22 03:44:50 -0500

    Threading for feed works? Needs more testing and also better error/success codes

--- servo_control.py
+++ servo_control.py
@@ -1,4 +1,5 @@
 from time import sleep
+from threading import Thread
 
 def _OpenFeedDoor() -> bool:
     return True
@@ -10,12 +11,21 @@ def _CloseFeedDoor() -> bool:
 
 def Feed(feed_time) -> bool:
     """Open and close the feeding door"""
+    t = Thread(target=_feed, args=[feed_time])
+    t.run()
+    print("returning from Feed")
+    return True
+
+
+def _feed(feed_time) -> bool:
     print("feeding...")
     result = _OpenFeedDoor()
     if not result:
         return False
-    
+    print("sleeping")   
+    sleep(0)
     sleep(feed_time)
+    print("end sleep")
 
     result = _CloseFeedDoor()
     print("fed")
