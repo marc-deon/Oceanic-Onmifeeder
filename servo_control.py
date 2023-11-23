41498575dc911eb393d3bf289888eef7600484ad
Timothy Moore <tmoor209@students.kennesaw.edu>
2023-11-23 15:21:09 -0500

    Implement servo control using scheduler rather than threads

--- servo_control.py
+++ servo_control.py
@@ -1,32 +1,29 @@
 from time import sleep
 from threading import Thread
+import schedule
+from datetime import timedelta
 
 def _OpenFeedDoor() -> bool:
+    print("OPENING FEED DOOR")
     return True
 
 
 def _CloseFeedDoor() -> bool:
+    print("CLOSING FEED DOOR")
     return True
 
 
 def Feed(feed_time) -> bool:
     """Open and close the feeding door"""
-    t = Thread(target=_feed, args=[feed_time])
-    t.run()
-    print("returning from Feed")
+    _OpenFeedDoor()
+    feed_time = timedelta(seconds=feed_time)
+
+    # Junk second
+    schedule.every().second.until(feed_time).do(_afterFeed).tag("feed")
     return True
 
 
-def _feed(feed_time) -> bool:
-    print("feeding...")
-    result = _OpenFeedDoor()
-    if not result:
-        return False
-    print("sleeping")   
-    sleep(0)
-    sleep(feed_time)
-    print("end sleep")
+def _afterFeed():
+    _CloseFeedDoor()
+    return schedule.CancelJob
 
-    result = _CloseFeedDoor()
-    print("fed")
-    return result
