4fb8b303f5e021e3eed018f9f95ab0032b794576
Marc d'Eon <highlyderivative.games@gmail.com>
2024-02-14 15:09:24 -0500

    Parallelize servo operations

--- servo_control.py
+++ servo_control.py
@@ -1,29 +1,50 @@
 from time import sleep
 from threading import Thread
-import schedule
-from datetime import timedelta
+
+#####################################################################
+#  FILE API  #
+##############
+#
+# Public:
+#   bool Feed(feed_time:int)
+#
+# Private:
+#   bool _OpenFeedDoor()
+#   None _CloseFeedDoor(feed_time)
+#
+#####################################################################
+
 
 def _OpenFeedDoor() -> bool:
+    """Private: Physically open door"""
+    # TODO: Open door here
     print("OPENING FEED DOOR")
     return True
 
 
-def _CloseFeedDoor() -> bool:
+# TODO: Rework messaging system to allow for this to 'return' a success
+def _CloseFeedDoor(feed_time:int) -> None:
+    """Private: Wait, and physically close door"""
+
+    # This for loop could be replaced to support float feed times
+    print("feeding for", feed_time, "...")
+    for i in range(feed_time):
+        print(("tock" if i%2 else "tick") + "...")
+        # Wait
+        sleep(1)
+
+    # TODO: Open door here
     print("CLOSING FEED DOOR")
-    return True
 
 
-def Feed(feed_time) -> bool:
-    """Open and close the feeding door"""
-    _OpenFeedDoor()
-    feed_time = timedelta(seconds=feed_time)
 
-    # Junk second
-    schedule.every().second.until(feed_time).do(_afterFeed).tag("feed")
-    return True
-
-
-def _afterFeed():
-    _CloseFeedDoor()
-    return schedule.CancelJob
-
+def Feed(feed_time:int) -> bool:
+    """Public: Open and close the feeding door in new thread"""
+    
+    # Open door
+    ok = _OpenFeedDoor()
+    # Open new thread to close door
+    t = Thread(target=_CloseFeedDoor, args=(feed_time,))
+    t.start()
+    # Return that we've succeeded in at least opening the door
+    return ok
