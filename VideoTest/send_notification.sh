3cfe6560e3c8ff054825fe14d8e983f98fc219ba
Marc d'Eon <highlyderivative.games@gmail.com>
2024-03-27 10:59:53 -0400

    dummy files for notifications

---
+++ VideoTest/send_notification.sh
@@ -0,0 +1,4 @@
+notify-send -i /home/maqo/senproj2023/VideoTest/feesh.png -u normal "Omnifeeder" "Normal Message"
+
+
+notify-send -i /home/maqo/senproj2023/VideoTest/feesh.png -u critical "Omnifeeder" "Critical Message"
