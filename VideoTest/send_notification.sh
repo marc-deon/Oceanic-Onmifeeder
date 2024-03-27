b209ce40b45683c5be78acd297400e001f30b875
Marc d'Eon <highlyderivative.games@gmail.com>
2024-03-27 11:13:24 -0400

    add link to reference document for godot.OS

--- VideoTest/send_notification.sh
+++ VideoTest/send_notification.sh
@@ -1,6 +1,6 @@
 cwd=$(pwd)
+# https://docs.godotengine.org/en/stable/classes/class_os.html#class-os-method-execute
+notify-send -i $cwd/feesh.png -u critical "Omnifeeder" "your fish are\nkinda fucked up bro"
 
-notify-send -i $cwd/feesh.png -u normal "Omnifeeder" "Normal Message"
 
-
-notify-send -i $cwd/feesh.png -u critical "Omnifeeder" "Critical Message"
+notify-send -i $cwd/feesh.png -u normal "Omnifeeder" "your fish seem fine"
