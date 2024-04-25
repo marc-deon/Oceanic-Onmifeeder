28c76c6adf0e03bd74d39808dd8fe9918ffafd2e
Ikram <ikram10234@gmail.com>
2024-04-25 18:40:03 -0400

    add bash shebang to launch scripts

--- launch-camera.sh
+++ launch-camera.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 #rpicam-vid --framerate 20 --width 640 --height 480 -t 0 --inline -o udp://127.0.0.1:6969 &
 
 # Several errors can be fixed by doing `fuser /dev/video0` and closing the shown processes
